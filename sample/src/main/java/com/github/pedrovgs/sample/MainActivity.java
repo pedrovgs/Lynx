f6b2601d5c56c424aec0757f57194799b4e49841
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-19 00:23:46 +0100

    Change copyright to use 2015 version

--- sample/src/main/java/com/github/pedrovgs/sample/MainActivity.java
+++ sample/src/main/java/com/github/pedrovgs/sample/MainActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Pedro Vicente Gomez Sanchez.
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.github.pedrovgs.sample;
 
 import android.os.Bundle;
