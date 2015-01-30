63ed60d66f1637aaea246b05064dc32c6867457f
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-30 18:50:52 +0100

    Add first test to LynxActivity

---
+++ lynx/src/test/java/com/github/pedrovgs/lynx/LynxActivityTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.pedrovgs.lynx;
+
+import android.view.View;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+@Config(emulateSdk = 18) @RunWith(RobolectricTestRunner.class) public class LynxActivityTest {
+
+  private LynxActivity lynxActivity;
+
+  @Before public void setUp() {
+    lynxActivity = Robolectric.buildActivity(LynxActivity.class).create().resume().get();
+  }
+
+  @Test public void shouldShowLynxViewAsVisible() {
+    LynxView lynxView = (LynxView) lynxActivity.findViewById(R.id.lynx_view);
+
+    int lynxViewVisibility = lynxView.getVisibility();
+    
+    assertEquals(View.VISIBLE, lynxViewVisibility);
+  }
+}
