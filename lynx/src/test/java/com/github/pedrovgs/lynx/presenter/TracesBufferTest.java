fcc3769b02a0347211aa0dd4d7b4c1891aae3e02
pedrovgs <pedrovicente.gomez@gmail.com>
2015-02-01 15:17:45 +0100

    Add some tests to TracesBuffer

---
+++ lynx/src/test/java/com/github/pedrovgs/lynx/presenter/TracesBufferTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.pedrovgs.lynx.presenter;
+
+import com.github.pedrovgs.lynx.model.Trace;
+import com.github.pedrovgs.lynx.model.TraceLevel;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class TracesBufferTest {
+
+  private static final int ANY_BUFFER_SIZE = 20;
+
+  private TraceBuffer traceBuffer;
+
+  @Before public void setUp() {
+    this.traceBuffer = new TraceBuffer(ANY_BUFFER_SIZE);
+  }
+
+  @Test public void shouldReturnAnEmptyListByDefault() {
+    List<Trace> traces = traceBuffer.getTraces();
+
+    assertTrue(traces.isEmpty());
+  }
+
+  @Test public void shouldAddTracesIfTheNumberOfTracesIsLowerThanTheBufferSize() {
+    List<Trace> traces = generateTraces(ANY_BUFFER_SIZE - 1);
+
+    traceBuffer.add(traces);
+
+    assertEquals(traces, traceBuffer.getTraces());
+  }
+
+  @Test public void shouldAddAllTracesIfTheSizeOfTheListIsEqualsToTheBufferSize() {
+    List<Trace> traces = generateTraces(ANY_BUFFER_SIZE);
+
+    traceBuffer.add(traces);
+
+    assertEquals(traces, traceBuffer.getTraces());
+  }
+
+  @Test
+  public void shouldAddAllTracesIfAddTwoListsAndTheSumOfSizesIsEqualsOrLowerThanTheBuffersize() {
+    List<Trace> firstTraces = generateTraces(ANY_BUFFER_SIZE / 2);
+    List<Trace> secondTraces = generateTraces(ANY_BUFFER_SIZE / 2);
+
+    traceBuffer.add(firstTraces);
+    traceBuffer.add(secondTraces);
+
+    List<Trace> expectedTraces = new LinkedList<Trace>(firstTraces);
+    expectedTraces.addAll(secondTraces);
+    assertEquals(expectedTraces, traceBuffer.getTraces());
+  }
+
+  @Test public void shouldAddJustTheLastTracesIfTheSizeOfTheInputListIsBiggerThanTheBufferSize() {
+    TraceBuffer traceBuffer = new TraceBuffer(20);
+    List<Trace> traces = generateTraces(30);
+
+    traceBuffer.add(traces);
+
+    List<Trace> expectedTraces = generateTraces(10, 30);
+    assertEquals(expectedTraces, traceBuffer.getTraces());
+  }
+
+  private List<Trace> generateTraces(int numberOfTraces) {
+    return generateTraces(0, numberOfTraces);
+  }
+
+  private List<Trace> generateTraces(int initialValue, int finalValue) {
+    int size = finalValue - initialValue;
+    List<Trace> traces = new LinkedList<Trace>();
+    for (int i = initialValue; i < finalValue; i++) {
+      Trace generatedTrace = new Trace(TraceLevel.DEBUG, String.valueOf(i));
+      traces.add(generatedTrace);
+    }
+    return traces;
+  }
+}
